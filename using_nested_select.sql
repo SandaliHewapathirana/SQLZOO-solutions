8e179f2805c5208c5d4b25275029743f9c1420dc
Sandali Hewapathirana <153343714+SandaliHewapathirana@users.noreply.github.com>
2024-06-27 21:18:44 +0530

    Update using_nested_select.sql

--- using_nested_select.sql
+++ using_nested_select.sql
@@ -1,1 +1,26 @@
+--List each country in the same continent as 'Brazil'.
+SELECT name FROM world WHERE continent = 
+(SELECT continent 
+FROM world WHERE name = 'Brazil')
 
+--List each country and its continent in the same continent as 'Brazil' or 'Mexico'.
+SELECT name, continent FROM world
+WHERE continent IN
+  (SELECT continent 
+     FROM world WHERE name='Brazil'
+                   OR name='Mexico')
+
+
+--Show the population of China as a multiple of the population of the United Kingdom
+SELECT
+ population/(SELECT population FROM world
+             WHERE name='United Kingdom')
+  FROM world
+WHERE name = 'China'
+
+--Show each country that has a population greater than the population of ALL countries in Europe.
+--Note that we mean greater than every single country in Europe; not the combined population of Europe.
+  SELECT name FROM world
+ WHERE population > ALL
+      (SELECT population FROM world
+        WHERE continent='Europe')
